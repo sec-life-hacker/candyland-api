72efc4bb3339c25ca52da76f1a000aba66e2e984
Jason Hsieh <Jason.CY.Hsieh@ibm.com>
2023-04-27 23:54:51 +0800

    add: account seeder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,8 +11,10 @@ require_relative 'test_load_all'
 def wipe_database
   app.DB[:events].delete
   app.DB[:locations].delete
+  app.DB[:accounts].delete
 end
 
 DATA = {} # rubocop:disable Style/MutableConstant
+DATA[:accounts] = YAML.safe_load File.read('app/db/seeds/account_seeds.yml')
 DATA[:locations] = YAML.safe_load File.read('app/db/seeds/location_seeds.yml')
 DATA[:events] = YAML.safe_load File.read('app/db/seeds/event_seeds.yml')
