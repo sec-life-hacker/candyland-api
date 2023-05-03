bb044dd95a3990e95a596aeee66e834e112398a5
Jason Hsieh <Jason.CY.Hsieh@ibm.com>
2023-05-03 14:03:06 +0800

    refactor: split controllers

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -7,6 +7,7 @@ module Candyland
   # Web controller for Candyland API
   class Api < Roda
     plugin :halt
+    plugin :multi_route
 
     route do |routing|
       response['Content-Type'] = 'application/json'
@@ -15,117 +16,10 @@ module Candyland
         { message: 'CandylandAPI up at /api/v1' }.to_json
       end
 
-      @api_root = 'api/v1'
-      routing.on @api_root do
-        routing.on 'accounts' do
-          @account_route = "#{@api_root}/accounts"
-
-          routing.on String do |username|
-            # GET api/v1/accounts/[username]
-            routing.get do
-              account = Account.first(username:)
-              account ? account.to_json : raise('Account not found')
-            rescue StandardError
-              routing.halt 404, { message: error.message }.to_json
-            end
-          end
-
-          # POST api/v1/accounts
-          routing.post do
-            new_data = JSON.parse(routing.body.read)
-            new_account = Account.new(new_data)
-            raise('Could not save account') unless new_account.save
-
-            response.status = 201
-            response['Location'] = "#{@account_route}/#{new_account.id}"
-            { message: 'Account created', data: new_account }.to_json
-          rescue Sequel::MassAssignmentRestriction
-            Api.logger.warn "MASS-ASSIGNMENT:: #{new_data.keys}"
-            routing.halt 400, { message: 'Illegal Request' }.to_json
-          rescue StandardError => e
-            Api.logger.error 'Unknown error saving account'
-            routing.halt 500, { message: e.message }.to_json
-          end
-        end
-
-        routing.on 'locations' do
-          @location_route = "#{@api_root}/locations"
-
-          routing.on String do |location_id|
-            routing.on 'events' do
-              @event_route = "#{@api_root}/locations/#{location_id}/events"
-
-              # GET api/v1/locations/[location_id]/events/[event_id]
-              routing.on String do |event_id|
-                event = Event.where(location_id:, id: event_id).first
-                event ? event.to_json : raise('Event not found')
-              rescue StandardError => e
-                routing.halt 404, { message: e.message }.to_json
-              end
-
-              # GET api/v1/locations/[location_id]/events
-              routing.get do
-                output = { data: Location[location_id].events }
-                JSON.pretty_generate(output)
-              rescue StandardError => e
-                routing.halt 404, { message: e.message }.to_json
-              end
-
-              # POST api/v1/locations/[location_id]/events
-              routing.post do
-                new_data = JSON.parse(routing.body.read)
-                location = Location[location_id]
-                # new_event = Candyland::CreateEventForLocation.call(location_id, new_data)
-                new_event = location.add_event(new_data)
-                raise 'Could not save event' unless new_event
-
-                response.status = 201
-                response['Location'] = "#{@event_route}/#{new_event.id}"
-                { message: 'Event saved', data: new_event }.to_json
-
-              rescue Sequel::MassAssignmentRestriction
-                Api.logger.warn "MASS-ASSIGNMENT: #{new_data.keys}"
-                routing.halt 400, { message: 'Illegal Attributes' }.to_json
-
-              rescue StandardError
-                routing.halt 500, { message: 'Database error' }.to_json
-              end
-            end
-
-            # GET api/v1/locations/[location_id]
-            routing.get do
-              location = Location[location_id]
-              location ? location.to_json : raise('Location not found')
-            rescue StandardError => e
-              routing.halt 404, { message: e.message }.to_json
-            end
-          end
-
-          # GET api/v1/locations
-          routing.get do
-            output = { data: Location.all }
-            JSON.pretty_generate(output)
-          rescue StandardError
-            routing.halt 404, { message: 'Could not find locations' }.to_json
-          end
-
-          # POST api/v1/locations
-          routing.post do
-            new_data = JSON.parse(routing.body.read)
-            new_location = Location.new(new_data)
-            raise('Could not save location') unless new_location.save
-
-            response.status = 201
-            response['Location'] = "#{@location_route}/#{new_location.id}"
-            { message: 'Location saved', data: new_location }.to_json
-
-          rescue Sequel::MassAssignmentRestriction
-            Api.logger.warn "MASS-ASSIGNMENT: #{new_location.keys}"
-            routing.halt 400, { message: 'Illegal Attributes' }.to_json
-
-          rescue StandardError => e
-            routing.halt 400, { message: e.message }.to_json
-          end
+      routing.on 'api' do
+        routing.on 'v1' do
+          @api_root = 'api/v1'
+          routing.multi_route
         end
       end
     end
