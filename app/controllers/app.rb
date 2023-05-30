41b07742d88a30279c212514c01572129779ffb4
Jason Hsieh <Jason.CY.Hsieh@ibm.com>
2023-05-30 20:04:02 +0800

    fix week13 issue

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -2,6 +2,7 @@
 
 require 'roda'
 require 'json'
+require_relative './helpers'
 
 module Candyland
   # Web controller for Candyland API
@@ -9,15 +10,21 @@ module Candyland
     plugin :halt
     plugin :multi_route
 
-    def secure_request?(routing)
-      routing.scheme.casecmp(Api.config.SECURE_SCHEME).zero?
-    end
+    include SecureRequestHelpers
 
     route do |routing|
       response['Content-Type'] = 'application/json'
 
       secure_request?(routing) ||
         routing.halt(403, { message: 'TLS/SSL secured Connection Required' }.to_json)
+
+      begin
+        @auth_account = authenticated_account(routing.headers)
+      rescue AuthToken::InvalidTokenError
+        routing.halt(403, { message: 'Invalid auth token' }.to_json)
+      rescue AuthToken::ExpiredTokenError
+        routing.halt(403, { message: 'Expired auth token' }.to_json)
+      end
 
       routing.root do
         { message: 'CandylandAPI up at /api/v1' }.to_json
---
+++ app/controllers/helpers.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Candyland
+  # Methods for controllers to mixin
+  module SecureRequestHelpers
+    class UnauthorizedRequestError < StandardError; end
+    class NotFoundError < StandardError; end
+
+    def secure_request?(routing)
+      routing.scheme.casecmp(Api.config.SECURE_SCHEME).zero?
+    end
+
+    def authenticated_account(headers)
+      return nil unless headers['AUTHORIZATION']
+
+      scheme, auth_token = headers['AUTHORIZATION'].split
+      return nil unless scheme.match?(/^Bearer$/i)
+
+      account_payload = AuthToken.new(auth_token).payload
+      Account.first(username: account_payload['attributes']['username'])
+    end
+  end
+end
