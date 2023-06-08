755e7625fa93f4c6bdf83bb1e33d05ea2ba53d88
Jason Hsieh <Jason.CY.Hsieh@ibm.com>
2023-06-08 14:39:17 +0800

    fix route

--- app/controllers/events.rb
+++ app/controllers/events.rb
@@ -15,15 +15,18 @@ module Candyland
       # GET api/v1/events/[event_id]
       routing.on String do |event_id|
         @req_event = Event.first(id: event_id)
-        event = GetEventQuery.call(auth: @auth, event: @req_event)
-        { data: event }.to_json
-      rescue GetEventQuery::ForbiddenError => e
-        routing.halt 403, { message: e.message }.to_json
-      rescue GetEventQuery::NotFoundError => e
-        routing.halt 404, { message: e.message }.to_json
-      rescue StandardError => e
-        puts "FIND EVENT ERROR: #{e.backtrace}"
-        routing.halt 500, { message: 'API server error' }.to_json
+
+        routing.get do
+          event = GetEventQuery.call(auth: @auth, event: @req_event)
+          { data: event }.to_json
+        rescue GetEventQuery::ForbiddenError => e
+          routing.halt 403, { message: e.message }.to_json
+        rescue GetEventQuery::NotFoundError => e
+          routing.halt 404, { message: e.message }.to_json
+        rescue StandardError => e
+          puts "FIND EVENT ERROR: #{e.backtrace}"
+          routing.halt 500, { message: 'API server error' }.to_json
+        end
       end
     end
   end
